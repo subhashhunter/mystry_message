1524fb87d836199cf56018671f3b41125a50064a
subhashhunter <160048175+subhashhunter@users.noreply.github.com>
2025-02-01 00:01:22 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,12 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  "rules":{
-  'react/
-no-unescaped-entities':'off'
-},
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
-export default eslintConfig;
